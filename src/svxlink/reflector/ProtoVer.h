e997f6cc5f2f31b8d5111380fe2dd4363ded133d
Chris G4NAB_F5VMR <g4nab.ne63@gmail.com>
2026-05-24 20:26:54 +0100

    update 2026

--- src/svxlink/reflector/ProtoVer.h
+++ src/svxlink/reflector/ProtoVer.h
@@ -6,7 +6,7 @@
 
 \verbatim
 SvxReflector - An audio reflector for connecting SvxLink Servers
-Copyright (C) 2003-2019 Tobias Blomberg / SM0SVX
+Copyright (C) 2003-2026 Tobias Blomberg / SM0SVX
 
 This program is free software; you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
